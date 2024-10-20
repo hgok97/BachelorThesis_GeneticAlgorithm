8df718d32e1a3846dfc90b135a8993cfff4075ec
Huseyin Gok <hgkdeveloper@gmail.com>
2024-10-20 17:45:41 +0200

    adding comment for TODO Information which includes roughly how the tasks for this algorithm look like.

---
+++ geneticalgorithmcvrp.py
@@ -0,0 +1,17 @@
+'''
+solving the CVRP for Garabage Collection
+with a genetic algorithm.
+
+TODO: Create a very basic/simple/naive GA to solve the Problem
+TODO: Generate a Dataset to Test Performance
+TODO: Research different ways crossover/mutation operators, representations in order to get a better solution
+TODO: Compare different operators and note any differences for the discussion part
+TODO: also build in some visualization for the route optimization
+TODO: incorporate the garbage collection problem into this... maybe include realword constraints?
+
+'''
+
+
+
+
+print("Hello World")
